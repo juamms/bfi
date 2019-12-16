2671a5d8c40b9af6e55c661ada72fedbcd1028cc
João Silva <joao.ms94@gmail.com>
2019-12-16 16:39:55 -0100

    Use structopt instead of just clap for command line argument parsing.
    Again, thanks to u/birkenfeld on r/rust.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,50 @@
-extern crate clap;
+extern crate structopt;
 
-use clap::{App, Arg};
 use io::Read;
 use machine::Machine;
+use std::path::PathBuf;
 use std::{fs, io};
+use structopt::StructOpt;
 
 mod machine;
 
+#[derive(StructOpt)]
+#[structopt(
+    name = "bfi",
+    version = "1.1.0",
+    about = "An experimental Brainfuck interpreter"
+)]
+struct Opt {
+    #[structopt(short, long, parse(from_os_str), help = "The file to execute")]
+    file: PathBuf,
+
+    #[structopt(short, long, help = "Optimise the program before running")]
+    optimise: bool,
+
+    #[structopt(short, long, help = "Execute the program step by step")]
+    step: bool,
+
+    #[structopt(short, long, help = "Use verbose output")]
+    verbose: bool,
+
+    #[structopt(
+        short,
+        long,
+        help = "Emits the intermediate representation of the program to the given FILE"
+    )]
+    emit: Option<PathBuf>,
+}
+
 fn main() {
-    let matches = App::new("bfi")
-        .version("1.1.0")
-        .about("An experimental Brainfuck interpreter")
-        .arg(
-            Arg::with_name("file")
-                .short("f")
-                .long("file")
-                .value_name("FILE")
-                .required(true)
-                .help("The file to execute")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("optimise")
-                .short("o")
-                .long("optimise")
-                .help("Optimise the program before running")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("step")
-                .short("s")
-                .long("step")
-                .help("Execute the program step by step")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .help("Use verbose output")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("emit")
-                .short("e")
-                .long("emit")
-                .help("Emits the intermediate representation of the program to the given FILE")
-                .value_name("FILE"),
-        )
-        .get_matches();
+    let opt = Opt::from_args();
+    let file = &opt.file;
 
-    let file = matches.value_of("file").unwrap();
-    let emit = matches.value_of("emit");
-    let optimise = matches.is_present("optimise");
-    let step = matches.is_present("step");
-    let verbose = matches.is_present("verbose");
-
-    if verbose {
-        println!("Reading raw program from '{}'...", file)
+    if opt.verbose {
+        println!("Reading raw program from {:?}...", file)
     }
 
     let raw_program: Vec<char> = fs::read_to_string(file)
-        .expect(&format!("File '{}' does not exist", file))
+        .expect(&format!("File '{:?}' does not exist", file))
         .chars()
         .filter(|c| ['>', '<', '+', '-', '.', ',', '[', ']'].contains(c))
         .collect();
@@ -69,13 +52,13 @@ fn main() {
     let raw_size = raw_program.len();
     let mut machine = Machine::new(30_000);
 
-    if verbose {
+    if opt.verbose {
         println!("Processing raw program...")
     }
 
-    machine.load_program(raw_program, optimise);
+    machine.load_program(raw_program, opt.optimise);
 
-    if verbose {
+    if opt.verbose {
         println!("Original program size: {} instructions", raw_size);
         println!(
             "Processed program size: {} instructions",
@@ -83,22 +66,22 @@ fn main() {
         );
     }
 
-    if let Some(emit_file) = emit {
-        if verbose {
-            println!("Emiting intermediate representation to '{}'...", emit_file);
+    if let Some(emit_file) = opt.emit {
+        if opt.verbose {
+            println!("Emiting intermediate representation to {:?}...", emit_file);
         }
 
         match fs::write(emit_file, machine.intermediate_representation()) {
             Ok(_) => {}
             Err(err) => {
-                if verbose {
+                if opt.verbose {
                     println!("Error writing intermediate representation file: {}", err)
                 }
             }
         }
     }
 
-    if step {
+    if opt.step {
         while !machine.has_program_ended() {
             machine.step();
             machine.dump();
